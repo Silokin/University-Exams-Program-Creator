7820d4e356bd134f6cf521c2548a836e58198231
Alexandra <alexvks22@users.noreply.github.com>
2019-12-15 21:08:21 +0200

    Add files via upload

---
+++ sample/src/test/java/gr/aueb/mscis/sample/service/AithousaServiceTest.java
@@ -0,0 +1,20 @@
+package gr.aueb.mscis.sample.service;
+
+import org.junit.Test;
+
+public class AithousaServiceTest {
+
+	@Test
+	public void findAllAithouses()
+	{}
+	
+	@Test
+	public void findAithousaById()
+	{}
+	
+	@Test
+	public void findAithousaByOrofos()
+	{}
+	
+	
+}
